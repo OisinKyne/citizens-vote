f4ce1987e0bc43b2ad72da774bd750c3cd8a5735
Oisin Kyne <kyneoisin@gmail.com>
2019-05-11 14:43:22 +0100

    Updates

---
+++ precache-manifest.d43d1fb46635a34ac799e7a37703fd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d186dc95bbdba3b19bf4fe0eb49bb8",
+    "url": "/citizens-vote/index.html"
+  },
+  {
+    "revision": "ab12d81e9398607e6fe8",
+    "url": "/citizens-vote/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/citizens-vote/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ab12d81e9398607e6fe8",
+    "url": "/citizens-vote/static/js/main.386b86cf.chunk.js"
+  },
+  {
+    "revision": "b50a8fcb7d6602c4bc97",
+    "url": "/citizens-vote/static/js/runtime~main.074be6c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/citizens-vote/static/media/logo.5d5d9eef.svg"
+  }
+]);
