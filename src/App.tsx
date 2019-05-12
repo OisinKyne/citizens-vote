213802056a3bfe6b40e60c9fa826285a22671aa7
Oisin Kyne <kyneoisin@gmail.com>
2019-05-12 16:00:18 +0100

    First typescript material ui page compiling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "./withRoot";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const styles = (theme: any) => ({
+  root: {
+    paddingTop: theme.spacing.unit * 20
+  }
+});
+
+class App extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="h4" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          example project
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Super Secret Password
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRoot(withStyles(styles)(App));
---
+++ src/Oireachtas/oireachtas.test.ts
@@ -0,0 +1,31 @@
+import Oireachtas from './oireachtas';
+
+describe('Oireachtas API object', () => {
+    let dail: Oireachtas;
+    let url: string;
+    let callback: Function;
+
+    beforeEach(()=>{
+        dail = new Oireachtas();
+        url = dail.prepareDailBillsRequestUrl();
+        callback = function (response: any) {
+            console.log('Callback was called. ')
+         };
+    })
+
+    it('prepareDailBillsRequestUrl returns the default url ', async function () {
+        const defaultUrl = 'https://api.oireachtas.ie/v1/legislation?bill_status=Current&bill_source=Government,Private%20Member&date_start=2019-05-01&date_end=2019-05-08&limit=50&chamber_id=&lang=en';
+        expect( dail.prepareDailBillsRequestUrl()).toEqual(defaultUrl);
+    });
+
+    it('prepareDailBillsRequestUrl can return a custom URL ', async function () {
+        const customUrl = 'https://api.oireachtas.ie/v1/legislation?bill_status=Current&bill_source=Government,Private%20Member&date_start=2019-05-05&date_end=2019-05-09&limit=49&chamber_id=&lang=ga';
+        expect(dail.prepareDailBillsRequestUrl('Current', '2019-05-05','2019-05-09', '49', '','ga')).toEqual(customUrl);
+    });
+
+    it('has a function called getDailBills that accepts a URL and callback ', async function () {
+        return dail.getDailBills(url, callback);
+    });
+
+
+})
