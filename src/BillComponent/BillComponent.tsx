aabd610e90f8e90cc4b41a644f35806d2cd7f740
Oisin Kyne <kyneoisin@gmail.com>
2019-05-31 20:30:59 +0100

    Add URL link to bill

--- src/BillComponent/BillComponent.tsx
+++ src/BillComponent/BillComponent.tsx
@@ -15,16 +15,30 @@ interface State {
  */
 class BillComponent extends React.Component<Props, State> {
   render() {
+    let billPdfUrl = "/";
+    if (this.props.bill.versions && this.props.bill.versions.length >= 1) {
+      billPdfUrl = this.props.bill.versions[0].version.formats.pdf.uri;
+    }
     return (
       <Grid item xs={12}>
         <Paper className={"billPaper"}>
           <Grid container justify="space-between" alignItems="center">
             <Grid item xs={10}>
-              <Typography variant="h6">
+              <Typography variant="h6" gutterBottom>
                 {this.props.bill.shortTitleEn}
               </Typography>
-              <Typography variant="body1">
+              <Typography variant="body1" gutterBottom>
                 {this.props.bill.longTitleEn}
+              </Typography>
+              <Typography variant="body1">
+                <a
+                  href={billPdfUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={"billPdfUrl"}
+                >
+                  Read Bill
+                </a>
               </Typography>
             </Grid>
             <Grid item xs={2}>
